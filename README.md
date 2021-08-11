b3e16f7a022f3c5f5eeeb25fb64bdc6c2a1eb6a1
Eric Kim <44147889+taehyunnkim@users.noreply.github.com>
2021-08-11 20:25:19 +0900

    Add pos and sentences

--- README.md
+++ README.md
@@ -1,58 +1,35 @@
-# GCIDE Dictionary
+# GCIDE XML Dictionary Parser
 
-[GCIDE dictionary](https://www.ibiblio.org/webster/) in json format. Note that this is not a complete version of GCIDE dictionary, it only has words and their definitions.
+**The GNU Collaborative International Dictionary of English (GCIDE)** is a free dictionary derived from _Webster's Revised Unabridged Dictionary 1913_. GCIDE is created by Patrick J. Cassidy and is maintained by volunteers from around the world. Though GCIDE corpus files use a special markup that requires the **gcider** utility, Michael Dyck converted these corpus files into [XML](https://www.ibiblio.org/webster/).
 
+Using the XML files, this program parses the data using Python. This project is an extension of [gcide-dictionary-json](https://github.com/aviaryan/gcide-dictionary-json) by [Avi Aryan](https://github.com/aviaryan)
 
-### Using
+## JSON Format
 
-The formatted dictionary json files, arranged alphabetically are inside the [json_files](json_files/) folder. 
+The formatted dictionary json files, arranged alphabetically are inside the [chapters](chapters/) folder. The all-in-one dictionary file is [dictionary.json](dictionary.json). Words that are included in the dictionary is inside [words.txt](words.txt).
 
-The compressed all-in-one file is [dictionary.json](dictionary.json).
-
-There is also a minimal form of dictionary.json which only has **one definition per word** (the first one). The file is [dictionary_minimal.json](dictionary_minimal.json).
-
-The basic structure of these files looks like
-
-```javascript
-"word1": [
-"definition 1",
-"definition 2"
-],
-"word2": [
-"definition 1"
-],
-"word3": [
-"definition 1",
-"definition 2",
-"definition 3",
-]
-// etc
-```
-
-### Customizing
-
-If you want to have control over the output json files, there are some variables in the [python script](gcide_parser.py) which can be configured. The comments should be sufficient to explain what each configuration is for.
-
-
-### Notes
-
-* The 'Same as word' or 'See word' type of definitions in dictionary have been replaced by ___word (3 underscores). This is controlled by the `fix_er` variable.
+The basic structure of these files look like the following:
 ```json
-"grains": [
-"___grain",
-"Pigeon's dung used in tanning.  See Grainer. n., 1"
-]
+{
+    "word1": [
+        {
+            'pos': 'Part of Speech'
+            'definition': 'Some definition'
+            'sentences': ['Some Sentence', 'Another sentence']
+        },
+        {
+            'pos': 'Another Part of Speech'
+            'definition': 'Another definition'
+            'sentences': ['Some Sentence', 'Another sentence']
+        }
+    ],
+    "word2": [
+        {
+            'pos': 'Part of Speech'
+            'definition': 'Some definition'
+            'sentences': ['Some Sentence', 'Another sentence']
+        }
+    ],
+    ...
+}
 ```
-
-* To cleanup generated dictionary, you can use variables like `only_alpha`, `remove_prefix` and `remove_as` from the [script](gcide_parser.py).
-
-
-### Bonus
-
-* You can use [dict.py](dict.py) as a command-line dictionary. It doesn't support word correction though.
-
-
-### Similar Projects
-
-* [adambom/dictionary](https://github.com/adambom/dictionary)
-* [javierjulio/dictionary](https://github.com/javierjulio/dictionary)
--- gcide_parser.py
+++ gcide_parser.py
@@ -1,84 +1,100 @@
 import json
 from bs4 import BeautifulSoup
 
+config_remove_prefix = True # remove words that don't start with alphabet . e.g. suffixes (-ing in i, -s in s)
+config_all_lowercase = True # make all words lowercase (not their definitions)
+config_only_alpha = False # words consisting of only alphabets allowed. e.g. removes words having spaces or dashes etc.
 
-indent = 0 # indent of output alphabet-wise json ; make it to None for single-line compressed output
-indent_main = None # indent of dictionary.json
-remove_as = False # remove example sentences from defn .. like 'as, a valuable item' . These give examples of how the word is used
-remove_prefix = False # remove words that don't start with alphabet . e.g. suffixes (-ing in i, -s in s)
-all_lowercase = True # make all words lowercase (not their definitions)
-only_alpha = False # words consisting of only alphabets allowed. e.g. removes words having spaces or dashes etc.
-
-fix_er = True # Fix entry reference,hyperlinks . convert (Bacteria : See Bacterium.) to (Bacteria : ___Bacterium)
-remove_orphans = True # remove words without definition
-ascii_output = False # if false, outputs unicode (recommended)
-
-gen_minimal = True
+config_remove_orphans = True # remove words without definition
+all_words = []
 
 
-def gcide_xml2json(xml, alphabet):
-	fname = xml
-	data = open(fname, 'r').read()
-	soup = BeautifulSoup(data, 'lxml')
-	dic = {}
-	ent = ''
-	ents = []
+def gcide_xml2json(file, letter):
+	with open(file, 'r') as xml:
+		data = xml.read()
+		soup = BeautifulSoup(data, 'lxml')
+		dic = {}
+		word = ''
+		words = []
+		pos = ''
 
-	for p in soup.find_all('p'):
-		if p.find('ent') != None:
-			delete_entry = max( remove_prefix and ent.lower().startswith(alphabet) == False and ent != '' ,
-							remove_orphans and ent != '' and len(dic[ent]) == 0 ,
-							only_alpha and ent != '' and ent.isalpha() == False )
-			if delete_entry:
-				for _ent in ents:
-					if _ent in dic: del dic[_ent] # band age and Band Age .. same when lowercase
+		for entry_block in soup('p'):
+			if entry_block.ent is not None:
+				checks = [
+					config_remove_prefix and not word.lower().startswith(letter),
+					# config_remove_orphans and word != '' and len(dic[word][0].data) == 0,
+					config_only_alpha and not word.isalpha()
+				]
 
-			ents = [i.get_text().lower() if all_lowercase else i.get_text() for i in p.find_all('ent')]
-			ents = [i for i in ents[:-1] if i[0:1].isalpha()] + [ents[-1]] # remove suffixes from same words cuz they are not my job
-			ent = ents[-1] # get the root word, last one
-			for _ent in ents[:-1]: # init all same words
-				dic[_ent] = ['___' + ent]
-			if ent not in dic:
-				dic[ent] = []
-		if ent == '':
-			continue
-		defn = p.find('def')
-		if defn != None:
-			defnt = defn.get_text()
-			if fix_er and defnt.find('See') * defnt.find('Same as') == 0 and defn.find('er') != None:
-				er = defn.find('er').get_text()
-				if all_lowercase:
-					er = er.lower()
-				dic[ent] += ['___' + er]
-			else:
-				if remove_as and defn.find('as') != None:
-					defn.find('as').clear()
-					defnt = defn.get_text()
-				dic[ent] += [defnt.strip(' ;.')]
+				delete_entry = any(checks)
 
-	return dic
+				if delete_entry:
+					for unwanted_word in words:
+						if unwanted_word in dic: del dic[unwanted_word] # band age and Band Age .. same when lowercase
+
+				words = [entry_word.get_text().lower() if config_all_lowercase else entry_word.get_text() for entry_word in entry_block.find_all('ent')]
+				word = words[-1] # Root word
+				all_words.append(word)
+				# entry_words = [entry_word for entry_word in entry_words[:-1] if entry_word[0].isalpha()] + word # remove suffixes
+
+				if entry_block.pos is not None:
+					pos = entry_block.pos.get_text()
+				else:
+					pos = ''
+
+				if word not in dic:
+					dic[word] = [
+						{
+							'pos': pos,
+							'data': []
+						}
+					]
+				else:
+					for data in dic[word]:
+						if data['pos'] != pos:
+							dic[word].append(
+								{
+									'pos': pos,
+									'data': []
+								}
+							)
+			
+			if word == '':
+				continue
+
+			def_entry = entry_block.find('def')
+			if def_entry is not None:
+				sentences = []
+				sentence_entry = def_entry.find('as')
+				if sentence_entry != None:
+					sentences = sentence_entry.get_text()[4:].split('; ') # Remove "as, " that starts the example sentence
+					sentence_entry.clear()
+
+				definitions = def_entry.get_text().strip(' ;.')
+
+				for word_data in dic[word]:
+					if word_data['pos'] == pos and len(definitions) != 0:
+						word_data['data'].append({
+							'definitions': definitions,
+							'sentences': sentences
+						})
+
+		return dic
 
 
 if __name__ == '__main__':
 	a2z = 'abcdefghijklmnopqrstuvwxyz'
-	# a2z = 's'
-	mdic = {}
-	for _ in a2z:
-		fname = 'xml_files/gcide_' + _ + '.xml'
-		print('Doing :', _)
-		mdic[_] = gcide_xml2json(fname, _)
-		open('json_files/gcide_' + _ + '.json', 'w', encoding='utf-8').write(json.dumps(mdic[_], ensure_ascii=ascii_output, indent=indent, sort_keys=True))
+	chapters = {}
+	for letter in a2z:
+		gcide_xml = 'xml_files/gcide_' + letter + '.xml'
+		print('Parsing gcide_%s.xml file...' % (letter))
+		chapters[letter] = gcide_xml2json(gcide_xml, letter)
 
-	fdic = {}
-	for _ in mdic:
-		fdic.update(mdic[_])
-	print('writing dictionary.json')
-	open('dictionary.json', 'w', encoding='utf-8').write(json.dumps(fdic, indent = indent_main, ensure_ascii=ascii_output, sort_keys=True))
+	dictionary = {}
+	for letter in chapters:
+		dictionary.update(chapters[letter])
 
-	if gen_minimal:
-		for i in fdic:
-			if len(fdic[i]) > 0:
-				fdic[i] = fdic[i][0]
-			else:
-				fdic[i] = ''
-		open('dictionary_minimal.json', 'w', encoding='utf-8').write(json.dumps(fdic, indent = indent_main, ensure_ascii=ascii_output, sort_keys=True))
+	print('Writing dictionary.json and words.json')
+	with open('dictionary.json', 'w', encoding='utf-8') as dictionary_file, open('words.txt', 'w', encoding='utf-8') as words:
+		json.dump(dictionary, dictionary_file, indent = True, ensure_ascii=True, sort_keys=True)
+		words.write("\n".join(all_words))
